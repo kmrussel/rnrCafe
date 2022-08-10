ec235fe36629b6e0fe6637def8338d0aa1ced12b
kmrussel <Katiemrussell28@gmail.com>
2022-08-10 02:03:56 -0700

    home added

--- app.py
+++ app.py
@@ -6,3 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template("home.html")
 
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+@app.route("/food-menu")
+def food():
+    return render_template("foodMenu.html")
+
+@app.route("/drinks-menu")
+def drinks():
+    return render_template("drinksMenu.html")
+
+@app.route("/about-us")
+def aboutUs():
+    return render_template("aboutUs.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
